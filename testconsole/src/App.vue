955baabd0a25b8420a6dc05edf978ea5b5e51b6d
Mike Zahalan <MikeZahalan@GMail.COM>
2024-02-25 15:54:39 -0800

    First App with Vue and Typescript

---
+++ testconsole/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('zelda')">change name</button>
+    <button @click="changeAge('30')">change age</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    const name = ref('Link');
+    const age = ref<number|string>(25);
+    return { name, age }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: string|number) {
+      this.age = age
+      return age;
+    }
+  }
+});
+</script>
+
+<style></style>
