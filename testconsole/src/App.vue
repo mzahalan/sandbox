699e3767fe6a98a7842ad526a0131b19033a2cbe
Mike Zahalan <MikeZahalan@GMail.COM>
2024-02-26 21:05:42 -0800

    Lesson 5

--- testconsole/src/App.vue
+++ testconsole/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app">
-    <JobList :jobs="jobs"/>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by Title</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+        <button @click="handleClick('location')">Order by Location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -8,6 +15,7 @@
 import { defineComponent, ref } from "vue";
 import JobList from "./components/JobList.vue";
 import Job from "./types/Job";
+import OrderTerm from './types/OrderTerm';
 
 
 export default defineComponent({
@@ -40,9 +48,36 @@ export default defineComponent({
         id: "4"
       },
     ]);
-    return {jobs};
+
+    const order = ref<OrderTerm>('title');
+
+    const handleClick = (term :OrderTerm) => {
+      order.value = term;
+    };
+
+    return {jobs, handleClick, order};
   }
 });
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+
+header .order {
+  margin-top: 20px;
+}
+
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+</style>
