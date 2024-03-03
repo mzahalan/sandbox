3a084243e1030aa5414404180e29895f79174d55
Mike Zahalan <MikeZahalan@GMail.COM>
2024-03-03 12:14:28 -0800

    Lesson 4 and some of lesson 5. Stopped here: https://youtu.be/KM1U6DqZf8M?si=iZsTzpTErzCWCemm&t=296
    nested modal component.

---
+++ vjs3_application/modal-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <Modal />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: "My First View App :)"
+    }
+  },
+  components: {
+    Modal
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
