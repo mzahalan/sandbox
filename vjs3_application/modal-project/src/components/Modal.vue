c91000ade5676a69d935780807bff3b27b0e5756
Mike Zahalan <MikeZahalan@GMail.COM>
2024-03-05 23:01:09 -0800

    Using Slots.

--- vjs3_application/modal-project/src/components/Modal.vue
+++ vjs3_application/modal-project/src/components/Modal.vue
@@ -1,19 +1,17 @@
 <template>
 <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{sale: theme === 'sale'}">
-        <h1 class="title">{{header}}</h1>
-        <p>{{text}}</p>
+        <slot>default content</slot>
+        <div class="actions">
+            <slot name="links"></slot>
+        </div>
     </div>
 </div>
 </template>
 
 <script>
 export default {
-    props: [
-        'header',
-        'text',
-        'theme'
-    ],
+    props: ['theme'],
     methods: {
         closeModal() {
             this.$emit('close')
@@ -45,11 +43,32 @@ export default {
 .modal p {
     font-style: normal;
 }
+
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
 .modal.sale {
     background: crimson;
     color: white;
 }
 .modal.sale h1 {
+    color: white;
+}
+
+.modal.sale .actions {
+    color: white;
+}
+.modal.sale .actions a {
     color: white;
 }
 </style>
