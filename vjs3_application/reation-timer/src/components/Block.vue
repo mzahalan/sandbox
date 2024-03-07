9ce5642abdc090741d43efac3f407a8c5404f5f0
Mike Zahalan <MikeZahalan@GMail.COM>
2024-03-06 20:51:20 -0800

    created a reaction_time game.

--- vjs3_application/reation-timer/src/components/Block.vue
+++ vjs3_application/reation-timer/src/components/Block.vue
@@ -1,10 +1,36 @@
 <template>
-    <div class="block">click me</div>
+    <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 
 <script>
 export default {
-    props: ['delay']
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            },10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(`Reaction Time: ${this.reactionTime}`)
+            this.$emit('gameOver',this.reactionTime)
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+            console.log(`Delay: ${this.delay}`)   
+        },this.delay)
+    }
 }
 </script>
 
--- vjs3_application/reation-timer/src/components/Results.vue
+++ vjs3_application/reation-timer/src/components/Results.vue
@@ -1,7 +1,42 @@
-<template></template>
+<template>
+    <div class="results">
+        <p>Results {{ score }}ms</p>
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
 
 
-<script></script>
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail Pace...'
+        }
+    }
+}</script>
 
 
-<style></style>
+<style>
+.results { 
+    font-size: 1.5em;
+    color: #333;
+    text-align: center;
+    padding: 20px 0;
+    font-weight: bold;
+}
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
