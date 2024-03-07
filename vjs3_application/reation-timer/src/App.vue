d0fed9dd551e738bdaeca8e03eed955b10be79fa
Mike Zahalan <mzahalan@amazon.com>
2024-03-06 16:59:11 -0800

    started tutorial 6 - reaction timer Created application with button and Block. Next step is lifecycle hooks: https://youtu.be/bc6czIBLKTg?si=8CBh6bUxlObrX0UH&t=769

---
+++ vjs3_application/reation-timer/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+export default {
+  name: 'App',
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random()*5000 // Random number between 2000ms and 7000ms
+      this.isPlaying = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
