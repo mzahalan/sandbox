905e2d3c8e7c2c2d4b672ec2630145b9570af321
Mike Zahalan <mzahalan@amazon.com>
2024-03-20 21:23:18 -0700

    Router Tutorial - up to https://youtu.be/juocv4AtrHo?si=xZKi-3P7e9lYdS0l&t=1153
    Next up is folder structure.

---
+++ vjs3_application/router-example/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eveniet a vitae veniam, soluta nisi doloribus nostrum perspiciatis cumque, aut facere corporis saepe maiores hic itaque nesciunt tempore libero architecto.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
---
+++ vjs3_application/router-example/src/views/LinksView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Links">
+    <h1>This page has links. (no it doesn't)</h1>
+  </div>
+</template>
