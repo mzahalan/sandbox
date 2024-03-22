f41cc64f6454b407800ef26e9d2e669b2750ec0b
Mike Zahalan <mzahalan@amazon.com>
2024-03-22 15:07:25 -0700

    Updated Jobs page to read from a database using fetch and json-server (simple api)

--- vjs3_application/router-example/src/views/jobs/JobDetails.vue
+++ vjs3_application/router-example/src/views/jobs/JobDetails.vue
@@ -1,16 +1,30 @@
 <template>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
+    <div v-if="job != null">
+        <h1>{{ job.title }}</h1>
+        <p>The job id is {{ job.id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading...</p>
+    </div>
 </template>
 
 <script>
 export default {
     props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+        return {
+            job: null
+        }
+    },
+
+    mounted() {
+        fetch(`http://localhost:3000/jobs/${this.id}`)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
+    }
+    // MDZ: npx json-server --watch data/db.json
 }
 </script>
 
