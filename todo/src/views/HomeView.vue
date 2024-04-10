670cdb15c04d47852ca32b78c8599571ee4dda1b
Mike Zahalan <MikeZahalan@GMail.COM>
2024-04-10 00:07:15 -0700

    There are so many differences between vuetify2 and vuetify3. This is going to be a tough tutorial.

--- todo/src/views/HomeView.vue
+++ todo/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <template>
-  Hello World
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+  <p class="bg-red text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto id numquam sapiente, delectus sint beatae cum! Non nisi ducimus aliquam ipsum vitae, porro asperiores commodi voluptatum, animi sunt alias deserunt accusamus quidem sit mollitia tempore repudiandae! Beatae deserunt labore adipisci exercitationem velit deleniti doloribus similique? Quam facilis fugit ipsum perferendis velit ad dolorem tempora, saepe in ducimus dignissimos officiis, esse ipsam. Inventore, ducimus. Explicabo commodi enim excepturi, hic tempora omnis, asperiores quia placeat dicta dolor natus rem perspiciatis et repellat quibusdam iure maiores ipsum distinctio alias voluptatum magni voluptatibus? Reiciendis illum molestiae fugit nam debitis nulla a doloremque voluptatum labore?</p>
+  <h1 class="text-h1">Massive Display</h1>
 </template>
 
 <script>
-// Components
-
-
 export default {
   name: 'HomeView',
   setup() {
