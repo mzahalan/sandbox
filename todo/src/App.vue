e593c6163873f421909b69f4a974a32d58d93b93
Mike Zahalan <MikeZahalan@GMail.COM>
2024-04-10 00:44:06 -0700

    made a nav drawer.

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -6,13 +6,14 @@
       prominent
       app
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-app-bar-title>Vuetify</v-app-bar-title>
       <v-spacer></v-spacer>
       <v-btn icon @click="toggleTheme">
         <v-icon>mdi-theme-light-dark</v-icon>
       </v-btn>
     </v-app-bar>
+    <NavDrawer v-model="drawer"/>
     <v-main>
       <router-view/>
     </v-main>
@@ -20,17 +21,19 @@
 </template>
 
 <script>
+import {ref} from 'vue'
 import {useTheme} from 'vuetify'
+import NavDrawer from '@/components/NavDrawer'
 export default {
   name: 'App',
-
+  components: {NavDrawer},
   setup() {
     const theme = useTheme()
-
+    const drawer = ref(false)
     const toggleTheme = () => {
       theme.global.name.value = theme.global.current.value.dark ? "light" : "dark"
     }
-    return { toggleTheme }
+    return { toggleTheme, drawer }
   },
 }
 </script>
---
+++ todo/src/components/NavDrawer.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-navigation-drawer>
+        <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+        <v-divider></v-divider>
+        <v-list density="compact">
+            <v-list-item><router-link :to="{name: 'home'}">Home</router-link></v-list-item>
+            <v-list-item><router-link :to="{name: 'about'}">About</router-link></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
